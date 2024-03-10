f255f09934360d96ab38a077d12ca0cb0e157e12
Nikita Noskov <noskovnikita@lenta.ru>
2024-03-10 10:55:23 +0300

    commit

---
+++ my_bot/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace my_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new TelegramBotClient("6271023416:AAGl8itNsfQ8KMpJ6uXozQ_X6OSJEb_wfv0");
+            client.StartReceiving(Update, Error);
+            Console.ReadLine();
+        }
+
+        async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+            if (message.Text != null)
+            {
+                Console.WriteLine($"{message.Chat.FirstName}     |     {message.Text}");
+
+                if (message.Text.ToLower().Contains("привет"))
+                {
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Привет.");
+                    return;
+                }
+            }
+
+            if (message.Photo != null)
+            {
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Хорошее изображение, но лучше отправь его документом.");
+                return;
+            }
+        }
+
+        private static Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
